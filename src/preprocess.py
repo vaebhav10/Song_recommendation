e01e169b34cc16ce841b5955a24cc43f3d56f9b6
Vaibhav Kumar Ram <vaibhavkumarr59@gmail.com>
2026-07-07 20:31:07 +0530

    Refactor data preprocessing and merge logic
    Updated duplicate removal logic and adjusted tag creation. Modified audio data merging to weight audio features.

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import StandardScaler, normalize
 import joblib
 
 
-
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 sc = StandardScaler()
 
@@ -14,11 +13,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 df = pd.read_csv(BASE_DIR/'data/dataset.csv')
 df=df.dropna()
-df.drop_duplicates(subset= ['track_id','artists', 'track_name','explicit'],inplace= True)
+
+df.drop_duplicates(subset= ['track_id'],inplace= True) 
+df.drop_duplicates(subset= ['artists', 'track_name','explicit'],inplace= True)
 
 df.drop(columns = {'Unnamed: 0','popularity', 'duration_ms', 'album_name'}, inplace = True)
 
-data = ['danceability','energy','key',	'loudness','mode',	'speechiness', 'acousticness', 'instrumentalness',	'liveness',	'valence',	'tempo',	'time_signature']
+data = ['danceability','energy','key',	'loudness','mode',	'speechiness', 'acousticness', 'instrumentalness',	'liveness',	'valence',	'tempo', 'time_signature']
 meta_data = df[data]
 df.drop(columns = data ,inplace =True)
 
@@ -27,9 +28,7 @@ for col in df.select_dtypes(include = ['str']).columns:
         continue
     df[col] = df[col].str.strip().str.lower()
     
-df['tags'] = df['artists']+ ' ' +df['track_genre']
-
-df[df.duplicated(subset=[ 'track_name','tags','explicit'])]
+df['tags'] = df['artists'].str.replace(';', ' ') + ' ' +df['track_genre'] 
 
 df = df.reset_index(drop=True)
 
@@ -38,7 +37,7 @@ tag_embeddings = model.encode(df['tags'].tolist(), normalize_embeddings = True)
 audio_data = sc.fit_transform(meta_data)
 audio_data = normalize (audio_data)
 
-merged = np.concatenate([audio_data, tag_embeddings], axis = 1)
+merged = np.concatenate([0.5* audio_data, tag_embeddings], axis = 1)
 merged = normalize(merged)
 np.save(BASE_DIR/'utils/merged_mat.npy',merged)
 # joblib.dump(df,BASE_DIR/'utils/df.pkl')
